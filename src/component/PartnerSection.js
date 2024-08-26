398cac8e5e530eb92b1b5c1713a0e4d3a6d3c0b2
arwaabuzgaia123 <arwaabuzgaia123@gmail.com>
2024-08-26 10:09:18 +0200

    PartnerSection

--- src/component/PartnerSection.js
+++ src/component/PartnerSection.js
@@ -1,58 +1,77 @@
-// src/components/PartnerSection.jsx
-
-import React from 'react';
-import { CiUser } from "react-icons/ci";
-import { PiGraduationCapLight } from 'react-icons/pi'; // Ensure this is the correct import for the icon
+import React, { useState } from 'react';
+import AllContests from "../component/AllContests";
+import TrainingCourses from "../component/TrainingCourses";
+import SeminarsAll from "../component/SeminarsAll";
+import ConferencesAll from "../component/ConferencesAll";
 
 const PartnerSection = () => {
-  const partners = [
-    { icon: <CiUser />, name: "وسائل الإعلام.شريك" },
-    { icon: <CiUser />, name: "وسائل الإعلام.شريك" },
-    { icon: <CiUser />, name: "وسائل الإعلام.شريك" },
-    { icon: <CiUser />, name: "وسائل الإعلام.شريك" },
-    { icon: <CiUser />, name: "وسائل الإعلام.شريك" },
-    { icon: <CiUser />, name: "وسائل الإعلام.شريك" },
-    { icon: <CiUser />, name: "وسائل الإعلام.شريك" },
-    { icon: <CiUser />, name: "وسائل الإعلام.شريك" },
-  ];
+  const [selectedSection, setSelectedSection] = useState("allContests");
+  const showMyContests = () => setSelectedSection("allContests");
+  const showMyTrainingCourses = () => setSelectedSection("TrainingCourses");
+  const showSeminars = () => setSelectedSection("allSeminars");
+  const showConferences = () => setSelectedSection("allConferences");
 
   return (
     <section 
-      className="bg-gray-100 py-10 px-6 rounded-lg" 
+      className="bg-gray-10 py-10 px-6 rounded-lg border border-gray-200" 
       style={{ 
-        marginTop: '200px',
-        width: '95%'
+        marginTop: '150px',
+        width: '95%',
+        height: '900px', // تحديد ارتفاع معين
+        overflowY: 'auto' // تفعيل التمرير العمودي
       }}
     >
-      <div className="text-center mb-6 top-10">
-        <div className="flex items-center justify-center mb-2">
-          <h2 
-            className="text-xl font-bold mr-2 text-custom-orange " 
-            style={{ fontFamily: 'Tajwal, sans-serif', direction: 'rtl' }}
+      <div
+        className="p-4"
+        style={{
+          fontFamily: "Tajwal, sans-serif",
+          direction: "rtl",
+          textAlign: "right",
+        }}
+      >
+        <h1 className="text-xl font-bold mb-1">النشاطات التى تقدمها المنصة</h1>
+        <h4 className="text-l font-bold text-gray-500 mr-5">اطلع على النشاطات وشارك الان ..  </h4>
+        <div className="flex mt-10 mr-8">
+          <button
+            className={`flex items-center mx-1 px-4 py-2 bg-blue rounded-lg text-l font-bold text-gray-900 hover:bg-custom-orange hover:text-white focus:outline-none ${
+              selectedSection === "allContests" ? "bg-custom-orange text-white" : "bg-gray-200"
+            }`}
+            onClick={showMyContests}
           >
-            الشريك الإعلامي
-          </h2>
-          <PiGraduationCapLight className="text-xl text-custom-orange" />
+            المسابقات
+          </button>
+          <button
+            className={`flex items-center mx-1 px-4 py-2 bg-blue rounded-lg text-l font-bold text-gray-900 hover:bg-custom-orange hover:text-white focus:outline-none ${
+              selectedSection === "TrainingCourses" ? "bg-custom-orange text-white" : "bg-gray-200"
+            }`}
+            onClick={showMyTrainingCourses}
+          >
+            الدورات التدريبية
+          </button>
+          <button
+            className={`flex items-center mx-1 px-4 py-2 bg-blue rounded-lg text-l font-bold text-gray-900 hover:bg-custom-orange hover:text-white focus:outline-none ${
+              selectedSection === "allConferences" ? "bg-custom-orange" : "bg-gray-200"
+            }`}
+            onClick={showConferences}
+          >
+            المؤتمرات
+          </button>
+          <button
+            className={`flex items-center mx-1 px-4 py-2 bg-blue rounded-lg text-l font-bold text-gray-900 hover:bg-custom-orange hover:text-white focus:outline-none ${
+              selectedSection === "allSeminars" ? "bg-custom-orange" : "bg-gray-200"
+            }`}
+            onClick={showSeminars}
+          >
+            الندوات
+          </button>
         </div>
-        <p 
-          className="text-lg" 
-          style={{ fontFamily: 'Tajwal, sans-serif', direction: 'rtl' }}
-        >
-          370 شريكًا إعلاميًا موثوقًا بهم
-        </p>
-      </div>
-      <div className="flex flex-wrap justify-center gap-4">
-        {partners.map((partner, index) => (
-          <div 
-            key={index} 
-            className="flex items-center gap-2 text-gray-700 p-2 "
-          >
-            <span  style={{ fontFamily: 'Tajwal, sans-serif', direction: 'rtl' }}>
-              {partner.name}
-            </span>
-            <span className="text-xl text-custom-orange">{partner.icon}</span>
-          </div>
-        ))}
+        {/* محتوى القسم */}
+        <div style={{ marginTop: '20px' }}>
+          {selectedSection === "allContests" && <AllContests />}
+          {selectedSection === "TrainingCourses" && <TrainingCourses />}
+          {selectedSection === "allSeminars" && <SeminarsAll />}
+          {selectedSection === "allConferences" && <ConferencesAll />}
+        </div>
       </div>
     </section>
   );
--- src/component/PromoSection.js
+++ src/component/PromoSection.js
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 const PromoSection = () => {
   const navigate = useNavigate();
   const handleAllactivity = () => {
-    navigate('/Allactivity');
+    navigate('/Login');
   };
   return (
     <section
