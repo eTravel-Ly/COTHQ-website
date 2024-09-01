e1fcf03a091f997fbdfa4db2b5df6a20dc75fc8a
arwaabuzgaia123 <arwaabuzgaia123@gmail.com>
2024-09-01 10:07:03 +0200

    all courses currentPage

--- src/component/AllCourses.js
+++ src/component/AllCourses.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import CourseImage from "../assets/images/ContinueWatchingSection1.png"; // Replace with your actual image import
 import noCoursesImage from "../assets/images/Search.png"; // صورة تعبيرية عند عدم وجود دورات
-import { FaRegUserCircle, FaSpinner } from "react-icons/fa"; // Import the star icon from react-icons
+import { FaRegUserCircle, FaSpinner, FaArrowLeft, FaArrowRight } from "react-icons/fa"; // Import arrow icons from react-icons
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { baseurl } from "../helper/Baseurl";
-
 
 export default function AllCourses() {
   const showPicCourses = async (fileName) => {
@@ -22,7 +21,9 @@ export default function AllCourses() {
   const [courses, setCourses] = useState([]);
   const [likedCourses, setLikedCourses] = useState({});
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(true); // حالة تحميل جديدة
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1); // Current page state
+  const coursesPerPage = 8; // Number of courses per page
 
   useEffect(() => {
     const fetchCourses = async () => {
@@ -42,7 +43,7 @@ export default function AllCourses() {
       } catch (error) {
         console.error("Error fetching courses:", error);
       } finally {
-        setLoading(false); // تعيين حالة التحميل إلى false بعد الانتهاء
+        setLoading(false);
       }
     };
 
@@ -76,10 +77,15 @@ export default function AllCourses() {
     }
   };
 
-  const rows = [];
-  for (let i = 0; i < courses.length; i += 4) {
-    rows.push(courses.slice(i, i + 4));
-  }
+  // Pagination logic
+  const indexOfLastCourse = currentPage * coursesPerPage;
+  const indexOfFirstCourse = indexOfLastCourse - coursesPerPage;
+  const currentCourses = courses.slice(indexOfFirstCourse, indexOfLastCourse);
+  const totalPages = Math.ceil(courses.length / coursesPerPage);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   const openCoursesDetails = (courseId) => {
     navigate(`/CoursesDetails/${courseId}`);
@@ -103,27 +109,24 @@ export default function AllCourses() {
           </p>
         </div>
       ) : (
-        <div className="flex h-screen  p-4">
+        <div className="flex flex-col items-center p-4">
           <div className="flex flex-wrap justify-center">
-            {rows.map((row, index) => (
-              <div key={index} className="flex flex-wrap mb-4 w-full max-w-6xl">
-                {row.map((course, idx) => (
-                  <div key={idx} className="w-full md:w-1/4 p-2">
-                    <div className="bg-white rounded-lg shadow-md p-3 h-auto flex flex-col justify-between">
-                      <img
-                        src={course.imageUrl || CourseImage}
-                        alt={course.title}
-                        className=" rounded-lg mb-3  h-40"
-                      />
-                     
-                     <div className="flex items-center mt-1 mb-2">
-                       
-                      <div className="flex-grow">
-                        <h3 className="text-md font-semibold mb-1 truncate">
-                          {course.title}
-                        </h3>
-                        <p  className="text-gray-600 text-xs"
-                         style={{
+            {currentCourses.map((course, idx) => (
+              <div key={idx} className="w-full md:w-1/4 p-2">
+                <div className="bg-white rounded-lg shadow-md p-3 h-auto flex flex-col justify-between">
+                  <img
+                    src={course.imageUrl || CourseImage}
+                    alt={course.title}
+                    className="rounded-lg mb-3 h-40"
+                  />
+                  <div className="flex items-center mt-1 mb-2">
+                    <div className="flex-grow">
+                      <h3 className="text-md font-semibold mb-1 truncate">
+                        {course.title}
+                      </h3>
+                      <p
+                        className="text-gray-600 text-xs"
+                        style={{
                           fontFamily: "Tajwal, sans-serif",
                           textAlign: "justify",
                           lineHeight: "1.5",
@@ -134,54 +137,92 @@ export default function AllCourses() {
                           display: "-webkit-box",
                           WebkitBoxOrient: "vertical",
                           WebkitLineClamp: 4,
-                        }}>
-                          {course.description}
-                           </p>
-                        <div className="flex items-center mt-1 mb-2">
-                          <FaRegUserCircle className="text-gray-600 " />
-                          <p
-                            className="text-xs text-gray-600  mr-2"
-                            style={{ fontFamily: "Tajwal, sans-serif" }}
-                          >
-                            {course.createdBy}
-                          </p>
-                        </div>
-                      </div>
-                     
-                    </div>
-                    <div className="flex items-center justify-between ">
-                        <div className="text-gray-600">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill={course.isFavorite ? "#ff3f52" : "none"}
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke={
-                              course.isFavorite ? "#ff3f52" : "currentColor"
-                            }
-                            className="w-6 h-6 cursor-pointer"
-                            onClick={() => handleLikeClick(course.id)}
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
-                            />
-                          </svg>
-                        </div>
-                        <button
-                          className="bg-custom-orange text-white px-4 py-2 rounded-3xl"
+                        }}
+                      >
+                        {course.description}
+                      </p>
+                      <div className="flex items-center mt-1 mb-2">
+                        <FaRegUserCircle className="text-gray-600 " />
+                        <p
+                          className="text-xs text-gray-600 mr-2"
                           style={{ fontFamily: "Tajwal, sans-serif" }}
-                          onClick={() => openCoursesDetails(course.id)}
                         >
-                          اشتر الآن
-                        </button>
+                          {course.createdBy}
+                        </p>
                       </div>
+                    </div>
                   </div>
+                  <div className="flex items-center justify-between">
+                    <div className="text-gray-600">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill={course.isFavorite ? "#ff3f52" : "none"}
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke={course.isFavorite ? "#ff3f52" : "currentColor"}
+                        className="w-6 h-6 cursor-pointer"
+                        onClick={() => handleLikeClick(course.id)}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                        />
+                      </svg>
+                    </div>
+                    <button
+                      className="bg-custom-orange text-white px-4 py-2 rounded-3xl"
+                      style={{ fontFamily: "Tajwal, sans-serif" }}
+                      onClick={() => openCoursesDetails(course.id)}
+                    >
+                      اشتر الآن
+                    </button>
                   </div>
-                ))}
+                </div>
               </div>
             ))}
+          </div>
+          {/* Pagination */}
+          <div className="mt-4">
+            <ul className="flex justify-center space-x-2 items-center">
+              <li>
+                <button
+                  className="px-3 py-1 rounded-full text-custom-orange"
+                  onClick={() =>
+                    currentPage > 1 && handlePageChange(currentPage - 1)
+                  }
+                  disabled={currentPage === 1}
+                >
+                  <FaArrowRight />
+                </button>
+              </li>
+              {Array.from({ length: totalPages }, (_, index) => (
+                <li key={index}>
+                  <button
+                    className={`px-3 py-1 rounded-full ${
+                      currentPage === index + 1
+                        ? "bg-custom-orange text-white"
+                        : "text-gray-700"
+                    }`}
+                    onClick={() => handlePageChange(index + 1)}
+                  >
+                    {index + 1}
+                  </button>
+                </li>
+              ))}
+              <li>
+                <button
+                  className="px-3 py-1 rounded-full text-custom-orange"
+                  onClick={() =>
+                    currentPage < totalPages && handlePageChange(currentPage + 1)
+                  }
+                  disabled={currentPage === totalPages}
+                >
+                   <FaArrowLeft />
+                 
+                </button>
+              </li>
+            </ul>
           </div>
         </div>
       )}
