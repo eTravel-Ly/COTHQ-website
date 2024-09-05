37d7ea7e04f45ee278e62580d13d5a644187d713
tahanielfituri <tahanilfituri@gmail.com>
2024-09-05 14:29:33 +0200

    edit

--- src/component/Allbooks.js
+++ src/component/Allbooks.js
@@ -250,15 +250,25 @@ const Allbooks = () => {
                             >
                               {availabilityStyle.btnText}
                             </button>
-                            <div
-                              className={`cursor-pointer ${
-                                likedBooks[book.id]
-                                  ? "text-red-500"
-                                  : "text-gray-500"
-                              }`}
-                              onClick={() => handleLikeClick(book.id)}
-                            >
-                              <CiHeart size={30} />
+                      
+                            <div className="text-gray-600">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill={book.isFavorite ? "#ff3f52" : "none"}
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke={
+                                  book.isFavorite ? "#ff3f52" : "currentColor"
+                                }
+                                className="w-6 h-6 cursor-pointer"
+                                onClick={() => handleLikeClick(book.id)}
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                                />
+                              </svg>
                             </div>
                           </div>
                         </div>
