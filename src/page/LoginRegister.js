00a8af7e7729c31e326873f69ece2ada0a8cc3bd
arwaabuzgaia123 <arwaabuzgaia123@gmail.com>
2025-01-20 12:05:25 +0200

    t

--- src/page/LoginRegister.js
+++ src/page/LoginRegister.js
@@ -41,7 +41,11 @@ const LoginRegister = () => {
    
       const response = await axios.post(baseurl + 'public/activation/request-otp',{
         email: values.email 
-      });
+      },
+      {
+       headers: {
+         'Content-Type': 'application/json'
+       }});
       if (response.status === 201 ) {
         setShowOTP(true);
         toast.success(translations.doneotp );
